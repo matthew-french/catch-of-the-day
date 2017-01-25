699fc77e10913adccc74a15579c5e777bee1bc86
matthew-french <matthew.french@gmail.com>
2017-01-25 19:43:44 +0000

    add authentication

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -78,8 +78,8 @@ class Inventory extends React.Component {
         <h2>Inventory</h2>
         <p>Sign in to manage your store's inventory</p>
         <button className="github" onClick={() => this.authenticate('github')}>Log In with Github</button>
-        <button className="facebook" onClick={() => this.authenticate('facebook')}>Log In with Github</button>
-        <button className="twitter" onClick={() => this.authenticate('twitter')}>Log In with Github</button>
+        <button className="facebook" onClick={() => this.authenticate('facebook')}>Log In with Facebook</button>
+        <button className="twitter" onClick={() => this.authenticate('twitter')}>Log In with Twitter</button>
       </nav>
     )
   }
